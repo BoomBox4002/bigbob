1561bd578f106a51933b73c6141459876f968df5
moron_diary <boombox4002@gmail.com>
2023-02-15 20:40:19 +0500

    v1

---
+++ app/src/main/java/com/example/bigbob/bigbob2.kt
@@ -0,0 +1,11 @@
+package com.example.bigbob
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class bigbob2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bigbob2)
+    }
+}
